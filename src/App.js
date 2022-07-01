c2c36a9c4497826963cac0d8e1f266ffe3e6edae
saarahchairi <sarni555@hotmail.com>
2022-07-01 16:17:34 +0200

    exo done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 class App extends React.Component{
+  state = {
+    heureOjd : new Date()
+  }
+  componentDidMount(){
+    setInterval(() => 
+    this.setState({
+      heureOjd : new Date()
+    })
+    ,1000)
+  }
   render(){
     return(
-      <div></div>
+      <div>
+        <p>Il est {this.state.heureOjd.getHours()}:{this.state.heureOjd.getMinutes()}:{this.state.heureOjd.getSeconds()} sec</p>
+      </div>
     );
   }
 }
